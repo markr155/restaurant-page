9e41d8f34b216e4db187ca59d514cc7bf873fea0
markr155 <markricho155@gmail.com>
2023-09-10 15:17:53 +1000

    main page set up

--- src/webpage.js
+++ src/webpage.js
@@ -5,22 +5,42 @@ function createHeader() {
     const heading = document.createElement('h1');
     heading.textContent = "Noi's Thai Restaurant";
     header.appendChild(heading);
+    header.appendChild(createNavBar());
     return header;
 };
 
+function createNavBar() {
+    const navBar = document.createElement('div');
+    const navButtons = ['Home', 'Menu', 'About Us'];
+    navButtons.forEach(button => {
+        const newButton = document.createElement('button');
+        newButton.textContent = button;
+        newButton.classList.add('nav-button');
+        navBar.appendChild(newButton);
+    });
+    return navBar;
+};
+
 function createMain() {
-    console.log('main');
+    const mainContainer = document.createElement('div');
+    mainContainer.setAttribute('id', 'main');
+    return mainContainer;
 };
 
 function createFooter() {
-    console.log('footer');
+    const footer = document.createElement('div');
+    footer.classList.add('footer');
+    const footerText = document.createElement('p');
+    footerText.textContent = 'Copyright © Mark Richardson 2023';
+    footer.appendChild(footerText);
+    return footer;
 };
 
 function initialisePage() {
     const content = document.getElementById('content');
     content.appendChild(createHeader());
-    // content.appendChild(createMain());
-    // content.appendChild(createFooter());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
 };
 
 
